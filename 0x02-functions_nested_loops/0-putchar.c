39e55be709dca35a9e9f3a9a8bbd4f575ef2d8ab
FEBNIKS <frankeris01@gmail.com>
2023-02-22 05:17:09 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,20 @@
 #include "main.h"
 
 /**
- * main - Entr point
+ * main - Prints Holberton as a message.
  *
- * Description: A program that prints _putchar followed by a new line
- *
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char text[8] = "_putchar";
-	int i = 0;
+        int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+        int count, sz;
 
-	for (i = 0; i < 8; i++)
-	{
-		_putchar(text[i]);
-	}
-	_putchar('\n');
-	return (0);
+        sz = sizeof(str) / sizeof(int);
+        for (count = 0; count < sz; count++)
+        {
+                _putchar(str[count]);
+        }
+        _putchar('\n');
+        return (0);
 }
